917a0856b21b139f68c40ab2b01fa894538074c9
charanvunnam1997 <vunnamcharan3734@gmail.com>
2022-07-07 16:56:07 +0530

    this is the comation of REX-12113 and REX-12114 and mix

---
+++ src/Sample.java
@@ -0,0 +1,10 @@
+public class Sample {
+    public static void main(String args[])
+    {
+        System.out.println("selenium");
+        System.out.println("charan");
+        System.out.println("vunnam");
+        System.out.println("jyothi");
+        System.out.println("jyothi22");
+    }
+}
