b09a435d390303428e65e7345c36157093e341b0
charanvunnam1997 <vunnamcharan3734@gmail.com>
2022-07-08 08:27:17 +0530

    Add a name

--- src/Sample.java
+++ src/Sample.java
@@ -3,7 +3,8 @@ public class Sample {
     {
         System.out.println("selenium");
         System.out.println("charan");
-        System.out.println("jyothi");
+        System.out.println("jyothi22");
         System.out.println("jyo2");
+        Syetem.out.println("dhjghhhthtuie");
     }
 }
